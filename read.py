43998e12ccc6d7e5cb4f414c442a0adb90be4066
Carlos Miguel Sayao <carsayao@pdx.edu>
2019-11-22 17:51:29 -0800

    Tidied code for submission

--- read.py
+++ read.py
@@ -1,96 +1,75 @@
+# read.py assumes data sits in "./spambase/rawdata/"
+
 import numpy as np
 import sys
 import os
 
-DEBUG = False
-
 class Read:
 
-    def __init__(self, inputs, samples, samples_test, debug=False):
+    def __init__(self, inputs, samples, samples_test):
         self.INPUTS = inputs
         # Split samples into training and test set
         self.SAMPLES = samples
         self.SAMPLES_TEST = samples_test
-        DEBUG = debug
+        # File paths
         self.path = os.path.dirname(os.path.realpath(__file__))
         self.data_raw = self.path + "/spambase/rawdata/spambase.data"
-        # self.data_dat = self.path + "/spambase/data.dat"
-        self.train_dat = self.path + "/spambase/train.dat"
-        self.test_dat = self.path + "/spambase/test.dat"
 
     # Read raw data from spambase.data, shuffle, return train and test sets
     # train_set.shape: (2301, 58)
     # test_set.shape: (2300, 58)
     def read_raw(self):
+        # Read data into x
+        print("reading raw data...")
         x = np.loadtxt(fname=self.data_raw, delimiter=',')
         # Deep copy data and shuffle
+        print("shuffling and splitting...")
         x_shuffled = x.copy()
         np.random.shuffle(x_shuffled)
         # Split data into train and test sets
         train_set = np.copy(x_shuffled[:self.SAMPLES])
         test_set = np.copy(x_shuffled[self.SAMPLES:])
         # Keep reshuffling until we get 2 balanced sets
-        while self.check_balanced(train_set, test_set) == False:
+        spam = round(self.prior(x), 4)
+        while self.check_balanced(train_set, test_set, spam) == False:
             # Shuffle
+            print("shuffling and splitting...")
             np.copy(np.random.shuffle(x_shuffled))
             # Split into 2 halves
             train_set = np.copy(x_shuffled[:self.SAMPLES])
             test_set = np.copy(x_shuffled[self.SAMPLES:])
         # Check shapes
-        if DEBUG == True:
-            print(train_set.shape)
-            print(test_set.shape)
         return train_set, test_set
 
-    # TODO: do i need a separate shuffle function for train.py?
-    # def shuffle(self):
+    # Calculate prior prob on dataset
+    def prior(self, x):
+        num_spam = np.sum(x[:,[self.INPUTS]])
+        return num_spam / x.shape[0]
 
     # Make sure our split sets reflect statistics of full data set (+-~1%) 
-    def check_balanced(self, x, y):
+    def check_balanced(self, x, y, spam):
         x_pos = np.sum(x[:, [57]])
         y_pos = np.sum(y[:, [57]])
-        if DEBUG:
-            print("x_pos",x_pos)
-            print("y_pos",y_pos)
-        x_pos /= 2301
-        y_pos /= 2300
-        if x_pos >= .404:
-            print("x_pos was >= .404:", x_pos)
+        x_pos /= self.SAMPLES
+        y_pos /= self.SAMPLES_TEST
+        err = 0.01
+        print("  checking balance of sets falls within %s%% of original data set..." % (err*100))
+        print("  bounds", spam+err, spam-err)
+
+        if x_pos >= spam+err:
+            print("    woops, x_pos was >= %s: %s" % (spam+err, x_pos))
             return False
-        if x_pos <= .384:
-            print("x_pos was <= .384:", x_pos)
+        if x_pos <= spam-err:
+            print("    woops, x_pos was <= %s: %s" % (spam-err, x_pos))
             return False
-        if y_pos >= .404:
-            print("y_pos was >= .404:", y_pos)
+        if y_pos >= spam+err:
+            print("    woops, y_pos was >= %s: %s" % (spam+err, y_pos))
             return False
-        if y_pos <= .384:
-            print("y_pos was <= .384:", y_pos)
+        if y_pos <= spam-err:
+            print("    woops, y_pos was <= %s: %s" % (spam-err, y_pos))
             return False
-        print("train set pos", x_pos)
-        print(" test set pos", y_pos)
+
+        print(" ",round(x_pos,4), "train set spam")
+        print(" ",round(y_pos,4), "test set spam")
         print()
         return True
-
-    def read_to_dat(self, x, y):
-        # Create memmap pointer on disk and read into .dats
-        # Leave room for class attribute
-        fp0 = np.memmap(self.train_dat, dtype='float64',
-                        mode='w+', shape=(self.SAMPLES, self.INPUTS+1))
-        fp0[:] = x[:]
-        del fp0
-        fp1 = np.memmap(self.test_dat, dtype='float64',
-                        mode='w+', shape=(self.SAMPLES_TEST, self.INPUTS+1))
-        fp1[:] = y[:]
-        del fp1
-
-# def main():
-#     inputs = 57
-#     samples = 2301
-#     samples_test = 2300
-
-#     run = Read(inputs, samples, samples_test)
-#     x, y = run.read_raw()
-#     run.read_to_dat(train_set, test_set)
-
-# if __name__ == "__main__":
-#     main()
--- train.py
+++ train.py
@@ -1,17 +1,9 @@
+# train.py assumes data sits in "./spambase/rawdata/"
+
 import numpy as np
 from read import Read
 import sys
 import os
-import argparse
-import warnings
-warnings.filterwarnings("error")
-
-parser = argparse.ArgumentParser(description="Naive Bayes' Classifier")
-parser.add_argument('--debug', action='store_true',
-                    help="run with DEBUG flag set to True")
-args = parser.parse_args()
-args = vars(args)
-DEBUG = args["debug"]
 
 class Classify:
 
@@ -22,15 +14,16 @@ class Classify:
         self.SAMPLES_TEST = samples_test
         self.train_set = np.zeros((samples, inputs+1))
         self.test_set  = np.zeros((samples_test, inputs+1))
-        # Not spam
+        # P(~spam)
         self.p0 = 0
-        # Spam
+        # P(spam)
         self.p1 = 0
+        # Mean and std dev of train set by class
         self.train0_mean = np.zeros(self.INPUTS+1)
         self.train1_mean = np.zeros(self.INPUTS+1)
         self.train0_std  = np.zeros(self.INPUTS+1)
         self.train1_std  = np.zeros(self.INPUTS+1)
-        self.correct = 0
+        # Paths we are working from 
         self.path = os.path.dirname(os.path.realpath(__file__))
         self.train_dat = self.path + "/spambase/train.dat"
         self.test_dat  = self.path + "/spambase/test.dat"
@@ -39,17 +32,11 @@ class Classify:
         self.train_set = np.copy(x)
         self.test_set = np.copy(y)
 
-    def load_from_dat(self):
-        self.train_set = np.memmap(self.train_dat, dtype="float64", mode='r',
-                                   shape=(self.SAMPLES, self.INPUTS+1))
-        self.test_set  = np.memmap(self.test_dat, dtype="float64", mode='r',
-                                   shape=(self.SAMPLES_TEST, self.INPUTS+1))
-    
-    # Calculate probability of class
+    # Calculate probability of classes where p0 is prob(~spam)
     def priors(self):
+        # Sum class attributes of train and test sets
         x_pos = np.sum(self.train_set[:,[self.INPUTS]])
         y_pos = np.sum(self.test_set[:,[self.INPUTS]])
-        print("Number of spam",x_pos+y_pos)
         p1 = (x_pos+y_pos)/(self.SAMPLES+self.SAMPLES_TEST)
         self.p1 = p1
         p0 = 1-p1
@@ -76,36 +63,30 @@ class Classify:
         self.train0_std  = np.where(self.train0_std==0,.0001,self.train0_std)
         self.train1_std  = np.where(self.train1_std==0,.0001,self.train1_std)
     
+    # Nb classifier, acts like a wrapper
+    def nb(self):
+        # Confusion matrix for our test set
+        confusion_matrix = np.zeros((2,2))
+        # Iterate through our test set and push it through our classifier
+        for i in range(self.test_set.shape[0]):
+            self.prob(self.test_set[i], confusion_matrix)
+        self.derive_matrix(confusion_matrix)
+
     # Calculate probabilities
     def prob(self, sample, confusion_matrix):
         # Retain target to later determine error
         # Must turn to int to use as array index
         actual = int(sample[self.INPUTS])
 
-        # TODO: track down runtime warns
         # Sliced up to 57 (so it doesn't include class)
         #   arr0: probability distribution that sample attribute is class 0
         #   arr1: probability distribution that sample attribute is class 1
-
-        # arr0 = np.log((1/(np.sqrt(2*np.pi)*self.train0_std[:57]))
-        #                *np.exp(-1*(np.square(sample[:57]-self.train0_mean[:57])
-        #                        /(2*np.square(self.train0_std[:57])))))
-        # arr1 = np.log((1/(np.sqrt(2*np.pi)*self.train1_std[:57]))
-        #                *np.exp(-1*(np.square(sample[:57]-self.train1_mean[:57])
-        #                        /(2*np.square(self.train1_std[:57])))))
-
-        try:
-            arr0 = np.log((1/(np.sqrt(2*np.pi)*self.train0_std[:57]))
-                        *np.exp(-1*(np.square(sample[:57]-self.train0_mean[:57])
-                                /(2*np.square(self.train0_std[:57])))))
-            arr1 = np.log((1/(np.sqrt(2*np.pi)*self.train1_std[:57]))
-                        *np.exp(-1*(np.square(sample[:57]-self.train1_mean[:57])
-                                /(2*np.square(self.train1_std[:57])))))
-        except RuntimeWarning:
-            # import pdb; pdb.set_trace()
-            print('WARNING!')
-            print('train0_std',self.train0_std[:57])
-            print('train1_std',self.train1_std[:57])
+        arr0 = np.log((1/(np.sqrt(2*np.pi)*self.train0_std[:57]))
+                       *np.exp(-1*(np.square(sample[:57]-self.train0_mean[:57])
+                               /(2*np.square(self.train0_std[:57])))))
+        arr1 = np.log((1/(np.sqrt(2*np.pi)*self.train1_std[:57]))
+                       *np.exp(-1*(np.square(sample[:57]-self.train1_mean[:57])
+                               /(2*np.square(self.train1_std[:57])))))
 
         # Classify with priors
         #   prob0: probability sample is class 0
@@ -119,35 +100,7 @@ class Classify:
             prediction = 0
         else:
             prediction = 1
-        # If error == 0, add 1 to our correctness
-        if (actual-prediction) == 0:
-            self.correct += 1
         confusion_matrix[actual][prediction] += 1
-
-
-        if DEBUG == True:
-            print("prob0",prob0)
-            print("prob1",prob1)
-            print("t",actual)
-            print("y", prediction)
-            print("l",actual-prediction)
-            print()
-
-    # Nb classifier, acts like a wrapper
-    def nb(self):
-        # Confusion matrix for our test set
-        confusion_matrix = np.zeros((2,2))
-        # Iterate through our test set and push it through our classifier
-        for i in range(self.test_set.shape[0]):
-            self.prob(self.test_set[i], confusion_matrix)
-        self.derive_matrix(confusion_matrix)
-
-        # Make sure we have the correct number of class 0 and 1
-        if DEBUG == True:
-            x_pos = np.sum(self.train_set[:,[self.INPUTS]])
-            y_pos = np.sum(self.test_set[:,[self.INPUTS]])
-            print(x_pos, y_pos)
-            print("x_pos+y_pos",x_pos+y_pos)
 
     # Derive accuracy, precision, and recall on test set
     # Confusion matrix is formatted n x m where:
@@ -166,17 +119,11 @@ class Classify:
         # Recall (sensitivity, or true positive rate)
         recall = tp / (tp+fn)
         print()
-        if DEBUG:
-            print("conf mat type",type(confusion_matrix))
-            print("correct",self.correct,self.correct/2300)
-            print("accuracy",accuracy)
-            print("precision",precision)
-            print("recall",recall)
-        else:
-            print("correct  ",round(self.correct/self.SAMPLES_TEST*100,4),self.correct)
-            print("accuracy ",round(accuracy*100,4))
-            print("precision",round(precision*100,4))
-            print("recall   ",round(recall*100,4))
+        print(tp+tn,"correct")
+        print(round(accuracy,4),"accuracy")
+        print(round(precision,4),"precision")
+        print(round(recall,4),"recall")
+        print()
         print(confusion_matrix) 
 
 def main():
@@ -197,16 +144,14 @@ def main():
     # Load our shuffled train and test sets into classify instance
     run.load(x, y)
 
-    # Read two static, preshuffled arrays as our train and test sets
-    #   (for debugging)
-    # run.load_from_dat()
-
+    # Calculate priors
     p1, p0 = run.priors()
-    if DEBUG:
-        print("P(1) = %s" % p1)
-        print("P(0) = %s" % p0)
-        print("P(1) + P(0) = %s" % float(p1+p0))
+
+    # Calculate mean and std dev on train set
     run.mean_std()
+
+    # Run nb classifier
+    print("classifying...")
     run.nb()
 
 if __name__ == "__main__":
